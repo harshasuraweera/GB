93680fb82a3bbc1cd63d934d341816abe0c8b54b
tharunibaddegama <dumindyabadde@gmail.com>
2021-04-19 18:11:54 +0530

    Add products continue.

--- GB/src/com/ProductService.java
+++ GB/src/com/ProductService.java
@@ -1,5 +1,44 @@
 package com;
 
+import model.ManageProductModel;
+
+import java.sql.SQLException;
+//For REST Service
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+//For JSON
+import com.google.gson.*;
+//For XML
+import org.jsoup.*;
+import org.jsoup.parser.*;
+import org.jsoup.nodes.Document;
+
+
+
+@Path("/product_view")
 public class ProductService {
+	
+	ManageProductModel product = new ManageProductModel();
+	
+	
+	@POST
+	@Path("/AddProduct")
+	@Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+	@Produces(MediaType.TEXT_HTML)
+	public String addProduct(@FormParam("productId") String productId,
+		 @FormParam("title") String title,
+		 @FormParam("sDesc") String sDesc,
+		 @FormParam("lDesc") String lDesc,
+		 @FormParam("price") String price,
+		@FormParam("downloadLink") String downloadLink)
+	throws SQLException{
+		
+		String output = product.addProduct(productId,title,sDesc,lDesc,price,downloadLink);
+		return output;
+	}
+	
+	
+	
+	
 
 }
--- GB/src/model/ManageProductModel.java
+++ GB/src/model/ManageProductModel.java
@@ -1,5 +1,156 @@
 package model;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+import com.mysql.cj.xdevapi.Statement;
+
 public class ManageProductModel {
+	
+private Connection connect(){
+		
+		Connection conn = null;
+		try{
+			Class.forName("com.mysql.cj.jdbc.Driver"); 
+			conn = DriverManager.getConnection("jdbc:mysql://localhost:3030/productservise", "root", "Highschool23*");
+		}catch (Exception e){
+			e.printStackTrace();
+			System.out.println(e);
+		}
+			return conn;
+	}
+//Add product using a form
+public String addProduct(String productId,String title,String sDesc,String lDesc,String price,String downloadLink) throws SQLException {
+
+String output = "";
+Connection conn = connect();
+
+String addProductsql = "INSERT INTO products (productId,title, sDesc, lDesc,price,downloadLink) VALUES (?,?,?,?,?,?)";
+PreparedStatement pre = conn.prepareStatement(addProductsql);
+
+try {
+	
+	
+	
+	pre.setString(1, productId);
+	pre.setString(2, title);
+	pre.setString(3, sDesc);
+	pre.setString(4, lDesc);
+	pre.setString(5, price);
+	pre.setString(6,downloadLink);
+	
+
+	
+	pre.executeUpdate();
+	
+	output +="<form action='products/cart' method='post'>"
+			+ "<input type='hidden' name='productId' value="+productId+">"
+			+ "<input type='hidden' name='title' value="+title+">"
+			+ "<input type='hidden' name='sDesc' value="+sDesc+">"
+			+ "<input type='hidden' name='lDesc' value="+lDesc+">"
+			+ "<input type='hidden' name='price' value="+price+">"
+			+ "<input type='hidden' name='downloadLink' value="+downloadLink+">"
+			+ "<input class=\"btn btn-primary\" type=\"submit\" value='Add Products'></form>";
+	
+	
+	output +="</div>";
+	
+	
+	
+	
+	
+} catch (SQLException e) {
+	// TODO Auto-generated catch block
+	e.printStackTrace();
+}
+
+return output;
+
+}
+
+
+
+
+//fetch Products Details to a Table
+/*public String fetchAllProducts(String search){
+	
+	String sql = "";
+	String output = "";
+	Connection conn = connect();
+	
+	
+	
+	if (conn == null){
+		return "Error while connecting to the database for inserting."; 
+	}else {
+		
+		output += "<meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\"> <link rel=\"stylesheet\" href=\"https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css\" integrity=\"sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm\" crossorigin=\"anonymous\">";
+	
+		output += "<div class=\"container\"><div class='row' >";
+		
+		
+		
+		try {
+			
+			if(search.equals("all")) {
+				 sql = "SELECT * FROM products";
+			}else if(!search.equals("all")) {
+				 sql = "SELECT * FROM products WHERE productId LIKE '%"+search+"%' ";
+			}
+			
+			Statement stmt;
+			stmt = conn.createStatement();
+			ResultSet rs = stmt.executeQuery(sql);
+			
+			
+			while (rs.next())
+			{
+				String productId = rs.getString("productId");
+	        	String productTitle = rs.getString("title");
+	        	String shortDescription = rs.getString("sDesc");
+	        	String longDescription = rs.getString("lDesc");
+	        	String productPrice = rs.getString("price");
+	        	String productLink = rs.getString("downloadLink");
+	        	
+	        	
+	        	output +="<div class='col-md-4' style='padding-top: 15px;padding-bottom: 15px;'>";
+	        	
+	        	output += "<p>Product ID :"+productId+"<br>Description : "+shortDescription+"<br>Price : "+productPrice+".00 LKR<br>Total Sales : "+totalSales+"</p>";
+	        	
+	        	
+	        	//buttons
+	        	output +="<form action='products/cart' method='post'>"
+	        			+ "<input type='hidden' name='productId' value="+productId+">"
+	        			+ "<input type='hidden' name='productName' value="+productName+">"
+	        			+ "<input type='hidden' name='shortDescription' value="+shortDescription+">"
+	        			+ "<input type='hidden' name='quantity' value="+quantity+">"
+	        			+ "<input type='hidden' name='price' value="+productPrice+">"
+	        			+ "<input class=\"btn btn-primary\" type=\"submit\" value='Add to Cart'></form>";
+	        	
+	        	
+	        	output +="</div>";
+	        	
+	        	
+			}
+			
+			conn.close();
+			
+		} catch (SQLException e) {
+			output = "Error while fetching products";
+			System.err.println(e.getMessage());
+			e.printStackTrace();
+		}
+		
+		output += "</div></div>";
+			
+		
+	}
+	
+	return output;
+}*/
+
+
 
 }

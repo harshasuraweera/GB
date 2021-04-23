48abc773bb2703a0de1e5d23b1c6bc47f9882a6e
Harsha Manoj Suraweera <harshamanoj912@gmail.com>
2021-04-23 21:54:56 +0530

    master error fixed

--- GB/src/model/SubmitProjectsModel.java
+++ GB/src/model/SubmitProjectsModel.java
@@ -1,5 +1,13 @@
 package model;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Random;
+
 public class SubmitProjectsModel {
 
 	
